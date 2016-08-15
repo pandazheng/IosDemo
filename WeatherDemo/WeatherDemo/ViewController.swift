112d75a4770a05b84f3249078b9a67951aa8f1f3
pandazheng <543828315@qq.com>
2016-08-15 15:52:23 +0800

    JSonExport,Just Demo
    JSonExport,Just Demo

---
+++ WeatherDemo/WeatherDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherDemo
+//
+//  Created by panda zheng on 16/8/15.
+//  Copyright © 2016年 pandazheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
